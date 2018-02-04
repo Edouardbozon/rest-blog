628a27c5b5b5df4344a10b0932df8c6a6a7623d5
edouardbozon <bozonedouard@gmail.com>
2018-02-04 23:25:51 +0100

    Add markdown

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,102 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../environments/environment";
+
+export interface HalCollection<T> {
+  _links: {
+    item: { href: string }[];
+  };
+  _embedded: {
+    item: T & { _links: { self: { href: string } } }[];
+  };
+  itemsPerPage: number;
+  totaItem: number;
+}
+
+export interface Post {
+  // id: string;
+  title: string;
+  subtitle: string;
+  content: string;
+  author: string;
+  createdAt: Date;
+  updatedAt: Date;
+  tags: Tag[];
+}
+
+export interface Tag {
+  name: string;
+  // id: string;
+}
 
 @Component({
-  selector: 'eb-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "eb-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
-  title = 'eb';
+  postCollection: HalCollection<Post>;
+  tagCollection: HalCollection<Tag>;
+
+  draft: Post = {
+    title: "",
+    subtitle: "",
+    content: "",
+    author: "me",
+    tags: [],
+    updatedAt: new Date(),
+    createdAt: new Date(),
+  };
+
+  constructor(private http: HttpClient) {
+    this.fetch();
+  }
+
+  create() {
+    this.draft.updatedAt = new Date();
+    this.http
+      .post<Post>(environment.api + "/api/posts", this.draft)
+      .subscribe(response => {
+        this.draft = {
+          title: "",
+          subtitle: "",
+          content: "",
+          author: "me",
+          tags: [],
+          updatedAt: new Date(),
+          createdAt: new Date(),
+        };
+        this.fetch();
+      });
+  }
+
+  fetch() {
+    this.http
+      .get<HalCollection<Post>>(environment.api + "/api/posts")
+      .subscribe(postCollection => {
+        this.postCollection = postCollection;
+      });
+
+    this.http
+      .get<HalCollection<Tag>>(environment.api + "/api/tags")
+      .subscribe(tagCollection => {
+        this.tagCollection = tagCollection;
+      });
+  }
+
+  hasTag(givenTag: Tag, post: Post): boolean {
+    return post.tags.some(tag => givenTag.name === tag.name);
+  }
+
+  toggleTag(givenTag: Tag, post: Post): void {
+    if (this.hasTag(givenTag, post)) {
+      post.tags.splice(
+        post.tags.findIndex(tag => tag.name === givenTag.name),
+        1,
+      );
+    } else {
+      post.tags.push({ ...givenTag });
+    }
+  }
 }
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,20 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http"; // replaces previous Http service
+import { FormsModule } from "@angular/forms";
+import { MarkdownToHtmlModule } from "markdown-to-html-pipe";
+import { AppRoutingModule } from "./app-routing.module";
 
-import { AppRoutingModule } from './app-routing.module';
-
-import { AppComponent } from './app.component';
-
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    MarkdownToHtmlModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
